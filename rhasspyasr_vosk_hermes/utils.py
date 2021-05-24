4fbcf3c64d7fca4d3ca5d1bb9b25af0330a166b3
Michael Hansen <michael.hansen.24@us.af.mil>
2021-05-24 11:40:19 -0400

    Expand key files for model searching

--- rhasspyasr_vosk_hermes/utils.py
+++ rhasspyasr_vosk_hermes/utils.py
@@ -3,17 +3,23 @@ import typing
 from pathlib import Path
 
 
-def find_model_dir(base_dir: Path, key_dir="graph") -> typing.Optional[Path]:
+def find_model_dir(
+    base_dir: Path, key_dirs=("graph", "ivector"), key_files=("AUTHORS", "LICENSE")
+) -> typing.Optional[Path]:
     """Recursively search a directory for a Vosk model"""
     if not base_dir.is_dir():
         return None
 
-    for sub_dir in base_dir.iterdir():
-        if sub_dir.name == key_dir:
-            return base_dir
-
-        maybe_model_dir = find_model_dir(sub_dir)
-        if maybe_model_dir is not None:
-            return maybe_model_dir
+    for item in base_dir.iterdir():
+        if item.is_dir():
+            if item.name in key_dirs:
+                return base_dir
+            else:
+                maybe_model_dir = find_model_dir(item)
+                if maybe_model_dir is not None:
+                    return maybe_model_dir
+        elif item.is_file():
+            if item.name in key_files:
+                return base_dir
 
     return None
